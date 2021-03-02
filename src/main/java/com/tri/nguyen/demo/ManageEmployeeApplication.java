e5e951bca7e847f81198d90c5c6c39969c541002
Tri Nguyen <tri.nguyen@digicommercegroup.com>
2021-03-02 14:28:15 +0700

    Fix api not match when deployed to tomcat server

--- src/main/java/com/tri/nguyen/demo/ManageEmployeeApplication.java
+++ src/main/java/com/tri/nguyen/demo/ManageEmployeeApplication.java
@@ -2,9 +2,16 @@ package com.tri.nguyen.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ManageEmployeeApplication {
+public class ManageEmployeeApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ManageEmployeeApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManageEmployeeApplication.class, args);
